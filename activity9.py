d481f1718c69f3871b8da109673fc8099302cb61
kristineshanecortezjalbuena-spec <kristineshanecortezjalbuena@gmail.com>
2026-09-07 10:50:35 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,21 @@
+#relational operator activity
+
+x = 5
+y = 5
+
+password1 = "shane"
+password2 = "Shane"
+
+#1
+print( x > y)
+#2
+print( x < y)
+#3
+print( x >= y)
+#4
+print( x <= y)
+#5
+print( x== y)
+print(password1 == password2)
+#6 ! =
+print(password1 != password2)
