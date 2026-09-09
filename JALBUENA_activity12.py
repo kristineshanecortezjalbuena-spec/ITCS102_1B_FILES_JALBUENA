cc71afe1819a54859cc6cd7c1fea4e4f5ca92443
kristineshanecortezjalbuena-spec <kristineshanecortezjalbuena@gmail.com>
2026-09-09 11:18:51 +0800

    Add files via upload

---
+++ JALBUENA_activity12.py
@@ -0,0 +1,33 @@
+#multiple if and elif conditions
+
+#Create a pytho program that would capture age group
+
+name = input("Please input your name --->")
+
+age = int(input("Please input your age ---> "))
+
+if age >= 0 and age <=5 :
+	print("That age is considered as INFANT ") 
+
+elif age >= 0 and age <=5 :
+	print("That age is considered as INFANT ") 
+
+elif age >=6 and age <=12 :
+	print("That age is considered as KID ")
+
+elif age >=13 and age <=15 :
+	print("That age is considered as PRE TEEN")
+
+elif age >=16 and age <=19 :
+	print("That age is considered as TEENAGER ")
+
+elif age >=20 and age <=29 :
+	print("That age is considered as ADULT ")
+
+elif age >=59 and age <=150 :
+	print("age valid")
+
+
+
+
+
