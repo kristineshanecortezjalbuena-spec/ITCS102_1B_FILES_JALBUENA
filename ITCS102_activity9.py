89ff8c8f1c501b0d8cc8755a93e2c4a2bce648d4
kristineshanecortezjalbuena-spec <kristineshanecortezjalbuena@gmail.com>
2026-09-13 11:11:19 +0800

    Add files via upload

---
+++ ITCS102_activity9.py
@@ -0,0 +1,21 @@
+#relational operator activity
+
+x = 5
+y = 5
+
+pass1 = "luh"
+pass2 = "Luh"
+
+#1 
+print(x > y)
+#2 
+print(x < y)
+#3
+print(x >= y)
+#4
+print(x <= y)
+#5
+print(x== y)
+print(pass1 == pass2)
+#6 !=
+print(pass1 != pass2)
