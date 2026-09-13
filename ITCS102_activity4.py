45954a685dbe4ecc889ddaa0d10336ec4e4fe24b
kristineshanecortezjalbuena-spec <kristineshanecortezjalbuena@gmail.com>
2026-09-13 10:29:30 +0800

    Add files via upload

---
+++ ITCS102_activity4.py
@@ -0,0 +1,12 @@
+#escape sequence
+
+# \n - new line
+# \t - tab space
+# \r - carriage return / deprecatd (obsolete)
+# \b - back space 
+# \\ - inset slash
+# \" - insert double quotation
+
+phase1= "\t\tDo you only love me cause you have to, \n\t\t\t\tbaby, can't you see im hurtin'. \n\t\t\t\t\t\tI know it ends here, \n\t\t\t\t\t\t\t\t\tI guess knowing me more leads you to loving me less,,\naray ko po, kain po tayo team ryzza"
+
+print (phase1)
