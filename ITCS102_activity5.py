b840dc4932fcb913c504d5f735fbb273ac4d073c
kristineshanecortezjalbuena-spec <kristineshanecortezjalbuena@gmail.com>
2026-09-13 11:10:11 +0800

    Add files via upload

---
+++ ITCS102_activity5.py
@@ -0,0 +1,25 @@
+#operators activity
+
+a = 21
+b = 18
+
+sum = a + b
+diff = a - b 
+product = a * b
+floor_div = a // b 
+quotient = a / b 
+exponent = a ** b
+modulo = a % b
+
+print("the sum of ",a," and", b," is",sum)
+print("the difference of ",a," and", b," is",diff)
+print("the product of ",a," and", b," is",a*b)
+print("the floor_div of ",a," and", b," is",a//b)
+print("the exponent of ",a," and", b," is",a**b)
+print("the quotient of ",a," and", b," is",a/b)
+print("the modulo of ",a," and", b," is",a%b)
+
+
+
+
+
