27febd2c267c339f4d324de3c5e848ceb75fd132
kristineshanecortezjalbuena-spec <kristineshanecortezjalbuena@gmail.com>
2026-09-13 11:11:35 +0800

    Add files via upload

---
+++ ITCS102_activity10.py
@@ -0,0 +1,12 @@
+#basic if else
+
+username = "loveu"
+password = "loveusm"
+
+u = input("Input USERNAME ---> ")
+p = input("Input PASSWORD ---> ")
+
+if u == username and p == password:
+    print ("username and password correct")
+else:
+    print("access denied")
